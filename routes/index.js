2f35c40be9f09d85fbe3a81d93ee2c8898280178
User <zipper@zipper.moe>
2018-09-29 02:54:38 -0400

    jwt stuff

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = require('express-promise-router')();
+var jwt = require('jsonwebtoken');
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+const PUBLIC_KEY = process.env.PUBLIC_KEY.replace(/\\n/g, '\n');
+
+function jwtsign(role, username) {
+  return jwt.sign({
+    role: role,
+    user: username,
+  },
+  PRIVATE_KEY,
+  {
+    algorithm: 'RS256',
+    expiresIn: '30d',
+  });
+}
+
+function jwtdecode(token) {
+  return jwt.verify(token, PUBLIC_KEY);
+}
+
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
@@ -9,7 +30,6 @@ const pool = new Pool({
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-
   res.render('index');
 });
 
