96f3a0be2b846adc0bb501b60ede7d95a880a5df
User <zipper@zipper.moe>
2018-09-29 01:56:10 -0400

    postgres and ui changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
-var router = express.Router();
+var router = require('express-promise-router')();
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+
+  res.render('index');
 });
 
 module.exports = router;
