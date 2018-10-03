3ed7fbe47839ee330733619fdf91f265877f1156
User <zipper@zipper.moe>
2018-10-03 13:08:07 -0400

    donations table and relations

--- tabledefs.sql
+++ tabledefs.sql
@@ -16,9 +16,23 @@ CREATE TABLE managers () INHERITS(accounts);
 CREATE TYPE locationtype AS ENUM ('Drop Off', 'Store', 'Warehouse');
 
 CREATE TABLE locations (
+    id serial primary key,
     name text NOT NULL,
     type locationtype NOT NULL,
     latitude double precision NOT NULL,
     longitude double precision NOT NULL,
     address text NOT NULL,
     phone text NOT NULL);
+
+CREATE TYPE donationtype as ENUM ('Clothing', 'Hat', 'Kitchen', 'Electronics', 'Household', 'Other');
+
+CREATE TABLE donations (
+    id serial primary key,
+    locationid integer references locations (id) NOT NULL,
+    tstamp timestamp NOT NULL,
+    shortdescription text NOT NULL,
+    description text NOT NULL,
+    value money NOT NULL,
+    type donationtype NOT NULL,
+    comments text NULL,
+    image bytea NULL);
