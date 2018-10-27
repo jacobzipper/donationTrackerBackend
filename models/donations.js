ab0b235be1f5c7c9c43dff8b2d9d8abecd24ee1d
Maxim Salikhov <msalihov@gmail.com>
2018-10-27 15:52:54 -0400

    Actually change the enum values in the model

--- models/donations.js
+++ models/donations.js
@@ -1,77 +1,77 @@
 /* jshint indent: 1 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('donations', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    locationid: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'locations',
-        key: 'id'
-      }
-    },
-    tstamp: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    shortdescription: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    value: {
-      type: "MONEY",
-      allowNull: false
-    },
-    type: {
-      type: DataTypes.ENUM("Clothing","Hat","Kitchen","Electronics","Household","Other"),
-      allowNull: false
-    },
-    comments: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    image: {
-      type: "BYTEA",
-      allowNull: true
-    },
-    addedbyadmin: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      references: {
-        model: 'admins',
-        key: 'username'
-      }
-    },
-    addedbymanager: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      references: {
-        model: 'managers',
-        key: 'username'
-      }
-    },
-    addedbyemployee: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      references: {
-        model: 'employees',
-        key: 'username'
-      }
-    },
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
-  }, {
-    tableName: 'donations'
-  });
+    return sequelize.define('donations', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        locationid: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'locations',
+                key: 'id'
+            }
+        },
+        tstamp: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        shortdescription: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        value: {
+            type: "MONEY",
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.ENUM("CLOTHING", "HAT", "KITCHEN", "ELECTRONICS", "HOUSEHOLD", "OTHER"),
+            allowNull: false
+        },
+        comments: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        image: {
+            type: "BYTEA",
+            allowNull: true
+        },
+        addedbyadmin: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            references: {
+                model: 'admins',
+                key: 'username'
+            }
+        },
+        addedbymanager: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            references: {
+                model: 'managers',
+                key: 'username'
+            }
+        },
+        addedbyemployee: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            references: {
+                model: 'employees',
+                key: 'username'
+            }
+        },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+    }, {
+        tableName: 'donations'
+    });
 };
